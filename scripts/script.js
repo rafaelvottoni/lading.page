b1eda4ab792ed172095911e9af558b0a08b36a9d
rafaelvottoni <rafaelottoni@gmail.com>
2021-08-28 00:10:25 -0300

    project reformulation

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
+//Menu
 const menu = document.querySelector('.menu')
 const toggle = document.querySelector('.toggle')
 
 toggle.addEventListener('click', () => {
   menu.classList.toggle('show')
+})
+
+//Validade form
+const form = document.querySelector('form')
+
+form.addEventListener('submit', event => {
+  const input = document.querySelector('#link')
+  if (input.value === '') {
+    event.preventDefault()
+    input.setAttribute('placeholder', 'Digite um link!')
+    form.classList.add('error')
+    input.classList.add('error')
+  }
 })
